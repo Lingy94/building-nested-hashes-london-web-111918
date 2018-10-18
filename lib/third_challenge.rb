c3abfb7b330d70714a9639e00801ee5d8e515114
Jack Ling <j.lingyx@gmail.com>
2018-10-18 11:55:51 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,7 +12,7 @@ def third_challenge
       :hero => {
         :name => "Romeo",
         :age => "12",
-        :status => "alive",
+        :status => "alive",}
       :hero_friends => []
    }, 
    :capulet => {
@@ -27,7 +27,7 @@ def third_challenge
       :heroine => {
         :name => "Juliet",
         :age => "15",
-        :status => "alive",
+        :status => "alive",}
       :heroine_friends => []
    }
   }
